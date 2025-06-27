b9f023f40221f8796e70278f650d8ae93b0d6178
xiepg <xiepg@meixing.com>
2025-06-27 17:40:31 +0800

    code format

--- composeApp/src/desktopMain/kotlin/io/kapaseker/ytor/resource/Into.kt
+++ composeApp/src/desktopMain/kotlin/io/kapaseker/ytor/resource/Into.kt
@@ -9,35 +9,35 @@ import org.jetbrains.compose.resources.*
 
 
 @Composable
-fun StringResource.inString():String {
+fun StringResource.inString(): String {
     return stringResource(this)
 }
 
-suspend fun StringResource.getString():String {
+suspend fun StringResource.getString(): String {
     return getString(resource = this)
 }
 
 @Composable
-fun StringResource.inString(vararg formatArgs: Any):String {
+fun StringResource.inString(vararg formatArgs: Any): String {
     return stringResource(this, *formatArgs)
 }
 
-suspend fun StringResource.getString(vararg formatArgs: Any):String {
+suspend fun StringResource.getString(vararg formatArgs: Any): String {
     return getString(resource = this, *formatArgs)
 }
 
 @Composable
-fun StringArrayResource.inStringArray():List<String> {
+fun StringArrayResource.inStringArray(): List<String> {
     return stringArrayResource(this)
 }
 
 @Composable
-fun  DrawableResource.inPainter(): Painter {
+fun DrawableResource.inPainter(): Painter {
     return painterResource(this)
 }
 
 
-val Dp.toPxInt :Int @Composable get() =  with(LocalDensity.current) { this@toPxInt.toPx().toInt() }
-val Dp.toPx :Float @Composable get() =  with(LocalDensity.current) { this@toPx.toPx() }
-val Dp.toSp :TextUnit @Composable get() =  with(LocalDensity.current) { this@toSp.toSp() }
-val Int.px :Dp @Composable get() =  with(LocalDensity.current) { this@px.toDp() }
+val Dp.toPxInt: Int @Composable get() = with(LocalDensity.current) { this@toPxInt.toPx().toInt() }
+val Dp.toPx: Float @Composable get() = with(LocalDensity.current) { this@toPx.toPx() }
+val Dp.toSp: TextUnit @Composable get() = with(LocalDensity.current) { this@toSp.toSp() }
+val Int.px: Dp @Composable get() = with(LocalDensity.current) { this@px.toDp() }
